1f2b8c8fa5edf499ed6e1bdfdfc1754123719631
Yogesh <ysmworking@gmail.com>
2026-05-03 19:27:50 +0530

    login css

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -18,8 +18,10 @@ const SignupSchema = Yup.object().shape({
 });
 
 export const Login = () => (
-  <div className="login-page">
-    <h1 className="signin-heading">Sign In to view restaurants</h1>
+  <div className="login-page m-auto flex flex-col h-screen justify-center items-center space-y-4 md:space-y-8">
+    <h1 className="signin-heading text-3xl uppercase font-semibold text-amber-500">
+      Sign In to view restaurants
+    </h1>
     <Formik
       className="formik"
       initialValues={{
@@ -41,54 +43,78 @@ export const Login = () => (
       }}
     >
       {({ errors, touched }) => (
-        <Form className="formik-form">
-          <label htmlFor="firstName">First Name</label>
-          <Field
-            name="firstName"
-            id="firstName"
-            placeholder="First Name"
-            type="text"
-            autoComplete="current-first-name"
-          />
-          {errors.firstName && touched.firstName ? (
-            <div className="form-error-tag">{errors.firstName}</div>
-          ) : null}
+        <Form className="formik-form borde border-amber-500 rounded-md flex flex-col p-4 md:min-w-96 shadow-lg hover:shadow-2xl hover:shadow-amber-500 space-y-4 mx-auto">
+          <label
+            htmlFor="firstName"
+            className="text-md text-amber-500 font-semibold flex flex-col"
+          >
+            First Name
+            <Field
+              name="firstName"
+              id="firstName"
+              placeholder="First Name"
+              type="text"
+              autoComplete="current-first-name"
+              className="outline-0 shadow p-1 text-orange-500 placeholder:font-normal"
+            />
+            {errors.firstName && touched.firstName ? (
+              <div className="form-error-tag text-red-500">{errors.firstName}</div>
+            ) : null}
+          </label>
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field
-            name="lastName"
-            id="lastName"
-            placeholder="Last Name"
-            type="text"
-            autoComplete="current-last-name"
-          />
-          {errors.lastName && touched.lastName ? (
-            <div className="form-error-tag">{errors.lastName}</div>
-          ) : null}
+          <label
+            htmlFor="lastName"
+            className="text-md text-amber-500 font-semibold flex flex-col"
+          >
+            Last Name
+            <Field
+              name="lastName"
+              id="lastName"
+              placeholder="Last Name"
+              type="text"
+              autoComplete="current-last-name"
+              className="outline-0 shadow p-1 text-orange-500 placeholder:font-normal"
+            />
+            {errors.lastName && touched.lastName ? (
+              <div className="form-error-tag text-red-500">{errors.lastName}</div>
+            ) : null}
+          </label>
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="a@b.co"
-            type="email"
-            autoComplete="current-email"
-          />
-          {errors.email && touched.email ? (
-            <div className="form-error-tag">{errors.email}</div>
-          ) : null}
+          <label
+            htmlFor="email"
+            className="text-md text-amber-500 font-semibold flex flex-col"
+          >
+            Email*
+            <Field
+              id="email"
+              name="email"
+              placeholder="a@b.co"
+              type="email"
+              autoComplete="current-email"
+              className="outline-0 shadow p-1 text-orange-500 placeholder:font-normal"
+            />
+            {errors.email && touched.email ? (
+              <div className="form-error-tag text-red-500">{errors.email}</div>
+            ) : null}
+          </label>
 
-          <label htmlFor="password">Password</label>
-          <Field
-            id="password"
-            name="password"
-            placeholder="12345678"
-            type="password"
-            autoComplete="current-password"
-          />
-          {errors.password && touched.password ? (
-            <div className="form-error-tag">{errors.password}</div>
-          ) : null}
+          <label
+            htmlFor="password"
+            className="text-md text-amber-500 font-semibold flex flex-col"
+          >
+            Password*
+            <Field
+              id="password"
+              name="password"
+              placeholder="12345678"
+              type="password"
+              autoComplete="current-password"
+              className="outline-0 shadow p-1 text-orange-500 placeholder:font-normal"
+            />
+            {errors.password && touched.password ? (
+              <div className="form-error-tag text-red-500">{errors.password}</div>
+            ) : null}
+          </label>
           <Button type="submit" label="Submit" />
         </Form>
       )}
