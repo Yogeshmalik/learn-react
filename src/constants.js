e8766caa3312ade149379fa8d9f5afcf92bce918
Yogesh <ysmworking@gmail.com>
2026-05-09 21:55:18 +0530

    https://corsproxy.io/?

--- src/constants.js
+++ src/constants.js
@@ -802,10 +802,10 @@ export const IMG_CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const RESTAURANT_LIST_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6098012&lng=77.1001525&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6098012&lng=77.1001525&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RESTAURANT_DETAILS_URL =
-  "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6098012&lng=77.1001525&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6098012&lng=77.1001525&restaurantId=";
 
 // export const RESTAURANT_DETAILS_URL =
 //   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6098012&lng=77.1001525&restaurantId=722373";
